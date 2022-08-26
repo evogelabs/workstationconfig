3c6c3fcb228978a3061226f873c2ab9419f40f79
fdagostini <fdagostini@users.noreply.github.com>
2022-08-26 12:02:05 -0300

    Fix.

--- configs/aliases.sh
+++ configs/aliases.sh
@@ -3,3 +3,6 @@ alias ll="ls -lah"
 alias l="ls -lh"
 alias createdb="createdb -h localhost -U evoge_user"
 alias dropdb="dropdb -h localhost -U evoge_user"
+
+# alias mylocalip='sudo ifconfig | grep wlan0 -A 1 | grep inet | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | grep 192 -m 1'
+# alias myip='curl https://ipinfo.io/ip'
