40a45f79c8d58ad5969c3ce491f0b0e0c423cafc
Franciso Julio <francisco@inteliclin.com.br>
2022-09-13 16:47:42 -0300

    Fix.

---
+++ scripts/database/evg.database.local.dev.create-database.clinic.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+createdb -h localhost evoge_clinic_dev -p 5432 -U evoge_user
--- scripts/database/evg.database.local.dev.drop-and-restore.clinic.schema.sh
+++ scripts/database/evg.database.local.dev.drop-and-restore.clinic.schema.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 pg_restore --verbose --clean --if-exists --no-acl --no-owner \
-    -h localhost -U evoge_user -e evoge_clinic_dev \
-    -p 5432 \
+    -h localhost -U evoge_user -p 5432 -d evoge_clinic_dev \
     -x --no-owner --role=evoge_user $1
