a91d8473d14dec3f07708bcce07476512142eedb
fdagostini <fdagostini@users.noreply.github.com>
2023-01-02 16:45:48 -0300

    Fix.

--- configs/aliases.sh
+++ configs/aliases.sh
@@ -1,8 +1,6 @@
 alias ls="lsd"
 alias ll="ls -lah"
 alias l="ls -lh"
-alias createdb="createdb -h localhost -U evoge_user"
-alias dropdb="dropdb -h localhost -U evoge_user"
 
 # alias mylocalip='sudo ifconfig | grep wlan0 -A 1 | grep inet | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | grep 192 -m 1'
 # alias myip='curl https://ipinfo.io/ip'
--- configs/environment.sh
+++ configs/environment.sh
@@ -1,5 +1,5 @@
 
-export NPMRCFILE="$HOME/.npmrc"
+# export NPMRCFILE="$HOME/.npmrc"
 
 eval "$(mcfly init zsh)"
 
@@ -8,28 +8,34 @@ test -e "$HOME/.github.zsh" && source "$HOME/.github.zsh"
 
 # Meus scripts
 SCRIPTS_DIR="$HOME/workstationconfig/scripts"
-export PATH="$PATH:/usr/local/opt/postgresql@15/bin:$SCRIPTS_DIR/connections:$SCRIPTS_DIR/database:$SCRIPTS_DIR/dev:$SCRIPTS_DIR/docker:$SCRIPTS_DIR/gcp"
+export PATH="$SCRIPTS_DIR/connections:$SCRIPTS_DIR/database:$SCRIPTS_DIR/dev:$SCRIPTS_DIR/docker:$SCRIPTS_DIR/gcp:$PATH"
 
 case "$(uname -s)" in
   Darwin)
+    export PATH="$PATH:/usr/local/opt/postgresql@15/bin"
+
     source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
     source /usr/local/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-    if type brew &>/dev/null; then
-      FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
-      autoload -Uz compinit
-      compinit
-    fi
+    # if type brew &>/dev/null; then
+    #   FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+    #   autoload -Uz compinit
+    #   compinit
+    # fi
     # Node
-    export PATH="/usr/local/opt/node@18/bin:$PATH"
-    export LDFLAGS="-L/usr/local/opt/node@18/lib"
-    export CPPFLAGS="-I/usr/local/opt/node@18/include"
+    # export PATH="/usr/local/opt/node@18/bin:$PATH"
+    # export LDFLAGS="-L/usr/local/opt/node@18/lib"
+    # export CPPFLAGS="-I/usr/local/opt/node@18/include"
   ;;
 
   Linux)
-    # source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-    # source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-    # source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
-    # source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+    export NODE_OPTIONS="--max_old_space_size=8192"
+
+    # Scripts da Jetbrains
+    export JETBRAINS_DIR="$HOME/.local/share/JetBrains/Toolbox/scripts"
+    export PATH="$JETBRAINS_DIR:$PATH"
+
+    # Autenticação do Google Cloud
+    export GOOGLE_APPLICATION_CREDENTIALS="$HOME/gcpkeys/default.json"
   ;;
 
   *)
